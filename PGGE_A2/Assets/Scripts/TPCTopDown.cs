97a9cef0f5ef25b4a3cbacaeac652217390444a5
jasminee33 <jasminetan424@gmail.com>
2024-01-23 20:07:37 +0800

    Q1-Made the name consistent

--- PGGE_A2/Assets/Scripts/TPCTopDown.cs
+++ PGGE_A2/Assets/Scripts/TPCTopDown.cs
@@ -13,11 +13,14 @@ namespace PGGE
 
         public override void Update()
         {
-            // For topdown camera we do not use the x and z offsets.
-            Vector3 targetPos = mPlayerTransform.position;
-            targetPos.y += CameraConstants.CameraPositionOffset.y;
-            Vector3 position = Vector3.Lerp(mCameraTransform.position, targetPos, Time.deltaTime * CameraConstants.Damping);
-            mCameraTransform.position = position;
+            //Q1.consistent naming
+            // For topdown camera we do not use the x and z offsets
+            Vector3 targetPosition = mPlayerTransform.position;
+            targetPosition.y += CameraConstants.CameraPositionOffset.y;
+
+            //making codes more readable
+            Vector3 newPosition = Vector3.Lerp(mCameraTransform.position, targetPosition, Time.deltaTime * CameraConstants.Damping);
+            mCameraTransform.position = newPosition;
             mCameraTransform.rotation = Quaternion.Euler(90.0f, 0.0f, 0.0f);
         }
     }
